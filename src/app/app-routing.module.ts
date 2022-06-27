3f60dc6ff2dbee092eed961b5ef45cebc59e536c
Adam Dupuis <adamdupuis@laposte.net>
2022-06-27 14:44:07 +0200

    Connection Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConnectionComponent } from "./connection/connection.component";
+import {AppComponent} from "./app.component";
+import {AuthGuard} from "./helpers/authGuard";
 
 const routes: Routes = [
-  {path: 'connect', component: ConnectionComponent}
+  {path: 'connect', component: ConnectionComponent},
+  {path: '', component: AppComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {ConnectionComponent} from "./connection/connection.component";
+import {InputTextModule} from "primeng/inputtext";
 import { LayoutComponent } from './layout/layout.component';
+import {ButtonModule} from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import {MessageModule} from "primeng/message";
 
 @NgModule({
   declarations: [
@@ -13,7 +18,13 @@ import { LayoutComponent } from './layout/layout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    AppRoutingModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]

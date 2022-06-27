9fb585311eaa51ee9cde9dd005cb9862a371a7f7
Adam Dupuis <adamdupuis@laposte.net>
2022-06-27 14:44:07 +0200

    Connection Page

--- src/app/connection/connection.component.ts
+++ src/app/connection/connection.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core'
+import {NgForm} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'connection',
@@ -7,12 +9,37 @@ import {Component, OnInit} from '@angular/core'
 })
 
 export class ConnectionComponent implements OnInit {
+  Users = [
+    {id: 'admin', password: 'admin'},
+    {id: 'root', password: 'root'}
+  ]
 
-  constructor() {
+  isShown: boolean = false
 
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+    if (localStorage.getItem('user') !== undefined) {
+      this.router.navigate([''])
+    }
+  }
 
+  toggleShow() {
+    this.isShown = true
+  }
+
+  onSubmit(form: NgForm) {
+    let loggedIn = false
+    this.Users.map(user => {
+      if (user.id === form.value.identifier && user.password === form.value.password) {
+        localStorage.setItem('user', form.value.identifier)
+        this.router.navigate([''])
+        loggedIn = true
+      }
+    })
+    if (!loggedIn) {
+      this.toggleShow()
+    }
   }
 }

9cd321a61104e1ab23f7e7a5f2aa6fe53d8680bb
Adam Dupuis <adamdupuis@laposte.net>
2022-06-27 14:44:07 +0200

    Connection Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ConnectionComponent} from "./connection/connection.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'connect', component: ConnectionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

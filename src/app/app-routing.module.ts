351b8410b7e5a71d19d2b63b9e9854b19cd85068
LisTrik49 <clem.moka49@gmail.com>
2022-06-27 15:13:16 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ConnectionComponent } from "./connection/connection.component";
 import {AppComponent} from "./app.component";
 import {AuthGuard} from "./helpers/authGuard";
+import {ModulesComponent} from "./modules/modules.component";
 
 const routes: Routes = [
   {path: 'connect', component: ConnectionComponent},
+  {path: 'modules', component: ModulesComponent, canActivate: [AuthGuard]},
   {path: '', component: AppComponent, canActivate: [AuthGuard] }
 ];
 

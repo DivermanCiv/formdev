5a9771cf1ffd68138c1e8a30ecedd400cea1dc1f
LisTrik49 <clem.moka49@gmail.com>
2022-06-30 15:38:26 +0200

    css signature done

--- src/app/signature/qrcode/qrcode.component.spec.ts
+++ src/app/signature/qrcode/qrcode.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QrcodeComponent } from './qrcode.component';
+import QrcodeComponent from './qrcode.component';
 
 describe('QrcodeComponent', () => {
   let component: QrcodeComponent;
